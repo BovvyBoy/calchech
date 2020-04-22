2d4c12122800c0eda995ab03797dc32c277a9f50
Daniel  Bovington <dbovington@macbook-pro-2.home>
2020-04-22 13:45:52 +0100

    Ingredient resource gen, namespaced, model done

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  
-  
-  
   namespace :api do
     namespace :v1 do
       resources :plans
       resources :days
       resources :meals
       resources :recipes
+      resources :ingredients
     end
   end
 end
