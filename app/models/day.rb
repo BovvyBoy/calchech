197a61a52eb6cf5870befdeddfdf0fcf186dcefe
Daniel  Bovington <dbovington@macbook-pro-2.home>
2020-04-22 13:24:41 +0100

    Day scaffold gen, namespaced, models done

---
+++ app/models/day.rb
@@ -0,0 +1,5 @@
+class Day < ApplicationRecord
+    belongs_to :plan
+    has_many :meals
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  namespace :Api do
-    namespace :V1 do
+  
+  namespace :api do
+    namespace :v1 do
       resources :plans
+      resources :days
     end
   end
 end
