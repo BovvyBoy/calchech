ed9f849dafa23d8369fed91ede5be7037157e11d
Daniel  Bovington <d.bovington@gmail.com>
2020-04-25 15:50:44 +0100

    route fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :plans
-      resources :days
-      resources :meals
-      resources :recipes
-      resources :ingredients
+      resources :plans do
+        resources :days
+      end
+      resources :meals do 
+        resources :ingredients
+      end
+      resources :ingredients do
+        resources :meals
+      end
     end
   end
 end
